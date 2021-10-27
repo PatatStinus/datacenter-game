580cdab5d67e054b78129813b56ca9e6cf56a870
InsaneTomboy <sumxxlove@gmail.com>
2021-10-27 16:12:44 +0200

    AddScore method + score UI

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,18 +7,36 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     public float timer;
+    public int score;
     [SerializeField] TMP_Text timeText;
+    [SerializeField] TMP_Text scoreText;
+
+    private void Start()
+    {
+        score = 0;       
+    }
 
     private void Update()
     {
+        CountDown();
+        scoreText.text = score.ToString();
+    }
+
+    private void CountDown()
+    {
         if (timer >= 0)
         {
+            timer -= Time.deltaTime;
+
             float minutes = Mathf.FloorToInt(timer / 60);
             float seconds = Mathf.FloorToInt(timer % 60);
 
             timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-
-            timer -= Time.deltaTime;
         }
+    }
+
+    public void AddScore(int s)
+    {
+        score += s;
     }
 }
