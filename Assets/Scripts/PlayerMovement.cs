8e3809df4f7e1242dbadfc90b73d7089215aaaf6
PatatStinus <71143317+PatatStinus@users.noreply.github.com>
2021-12-15 13:15:36 +0100

    Revert "Helemaal zelf het movement script geupdate"
    This reverts commit 6076f2e202e9aa257a88197ed1c1ece225310a77.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,30 +4,23 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float moveSpeed = 0.1f;
-    float horizontalMove = 0;
-    float verticalMove = 0;
-    public float zPos = 0;
-    public float scaleMod = 1;
-    GameObject character;
+    [SerializeField] private float speed;
+    private Rigidbody2D rb;
+    private Vector2 movement;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        character = gameObject;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    private void Update()
     {
-        horizontalMove = Input.GetAxis("Horizontal") * moveSpeed;
-        verticalMove = Input.GetAxis("Vertical") * moveSpeed;
-        character.transform.Translate(new Vector3(horizontalMove, verticalMove, 0));
-        float charY = Mathf.Clamp(character.transform.position.y, -10f, 0);
-        float charX = Mathf.Clamp(character.transform.position.x, -6, 6);
-        zPos = charY / -10f * -5;
-        character.transform.position = new Vector3(charX, charY, zPos);
-        scaleMod = (character.transform.position.y / -1.5f) / 4 + 0.25f;
-        character.transform.localScale = new Vector3(scaleMod, scaleMod, 1);
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * speed * Time.fixedDeltaTime);
     }
 }
