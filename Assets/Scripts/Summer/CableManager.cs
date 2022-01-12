0e752f5c0f40c24b7a350100e3d2a468e9314568
InsaneTomboy <sumxxlove@gmail.com>
2022-01-12 15:29:02 +0100

    Insanely scuffed start to cable management

--- Assets/Scripts/Summer/CableManager.cs
+++ Assets/Scripts/Summer/CableManager.cs
@@ -6,46 +6,66 @@ public class CableManager : MonoBehaviour
 {
     Transform hitCable;
 
-    RaycastHit2D hit;
+    //RaycastHit2D hit;
+
+    bool isHoldingCable;
 
     [SerializeField] LayerMask cableLayer;
 
+    [SerializeField] List<Transform> cables;
+    [SerializeField] List<Transform> cableConnectors;
+
+    public List<Transform> randomCables;
+
+    Collider2D target;
+
     private void Start()
     {
+        isHoldingCable = false;
         hitCable = null;
+
+        for (int i = 0; i < 4; i++)
+        {
+            int random = Random.Range(0, cables.Count - 1);
+            randomCables.Add(cables[random]);
+            cables.Remove(cables[random]);
+        }
     }
 
     private void Update()
     {
-        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray.origin, ray.direction, out RaycastHit hit, 1000, cableLayer) && randomCables.Contains(hit.transform))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                hitCable = hit.transform;
+                Debug.Log(hitCable);
+            }
+            else if (Input.GetMouseButtonUp(0))
+                hitCable = null;
 
-        //hit = Physics2D.Raycast(Input.mousePosition, ray.direction, cableLayer);
+            if (hitCable == null)
+                return;
 
-        //if (hit)
-        //{
-        //    hitCable = hit.transform;
+            hitCable.position = new Vector3(hit.point.x, hit.point.y, hitCable.position.z);
+        }
 
-        //    Debug.Log(hit.transform.name);
-        //    if (Input.GetMouseButton(0))
-        //    {
-        //        hitCable.position = new Vector3(hit.point.x, hit.point.y, hitCable.position.z);
-        //    }
-        //    else if (Input.GetMouseButtonUp(0))
-        //        hitCable = null;
-        //}
-
-
-        //if (mousePos != null)
-        //{
-        //    hitCable = hit.transform;
-
-        //    Debug.Log(hit.transform.name);
-        //    if (Input.GetMouseButton(0))
-        //    {
-        //        hitCable.position = new Vector3(hit.point.x, hit.point.y, hitCable.position.z);
-        //    }
-        //    else if (Input.GetMouseButtonUp(0))
-        //        hitCable = null;
-        //}
+        for (int j = 0; j < randomCables.Count; j++)
+        {
+            for (int i = 0; i < cableConnectors.Count; i++)
+            {
+                if (randomCables[j] == hitCable && j == i)
+                {
+                    if (Vector3.Distance(cableConnectors[i].position, hitCable.position) < 2f)
+                    {
+                        //hitCable.position = cableConnectors[i].position;
+                        randomCables.Remove(randomCables[i]);
+                        cableConnectors.Remove(cableConnectors[i]);
+                        hitCable = null;
+                    }
+                }
+            }
+        }
     }
 }
